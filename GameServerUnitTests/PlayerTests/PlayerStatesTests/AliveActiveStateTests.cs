3b42ed94d9979a635f3b0f3b4bc48938946f0e87
bogse <ciucuras.bogdan@gmail.com>
2023-02-02 10:33:00 +0200

    Add GameServer Unit test folder structure

---
+++ GameServerUnitTests/PlayerTests/PlayerStatesTests/AliveActiveStateTests.cs
@@ -0,0 +1,20 @@
+﻿using GameServer.Player.PlayerStates;
+
+namespace GameServerUnitTests.PlayerTests.PlayerStatesTests
+{
+    [TestClass]
+    public class AliveActiveStateTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            // Arrange
+            var aliveActiveState = new AliveActiveState();
+
+            // Act
+
+            // Assert
+            Assert.Fail("Test should fail because the class is empty");
+        }
+    }
+}
