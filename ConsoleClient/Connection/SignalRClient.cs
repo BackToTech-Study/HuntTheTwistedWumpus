49b3daa79cefc9f6346085fb5952a767a2e39c4d
bogse <ciucuras.bogdan@gmail.com>
2023-02-13 09:29:36 +0200

    Created appsettings.json for BaseUrl

--- ConsoleClient/Connection/SignalRClient.cs
+++ ConsoleClient/Connection/SignalRClient.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleClient.Connection
 {
@@ -6,18 +7,28 @@ namespace ConsoleClient.Connection
     {
         public HubConnection _playerHubConnection;
         public HubConnection _caveHubConnection;
+        private readonly string _baseUrl;
+
         public SignalRClient()
         {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _baseUrl = configuration.GetSection("SignalR")["BaseUrl"];
+
             _playerHubConnection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:7252/playerhub")
+                .WithUrl(_baseUrl + "/playerhub")
                 .WithAutomaticReconnect()
                 .Build();
 
             _caveHubConnection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:7252/cavehub")
+                .WithUrl(_baseUrl + "/cavehub")
                 .WithAutomaticReconnect()
                 .Build();
         }
+
         public async Task Connect() 
         {
             try
