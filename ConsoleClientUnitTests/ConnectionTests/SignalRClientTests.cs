cc58b61b6214ceda17e87278942f4a180d379af1
bogse <ciucuras.bogdan@gmail.com>
2023-02-15 08:43:14 +0200

    Refactor baseUrl with DI and integration test

--- ConsoleClientUnitTests/ConnectionTests/SignalRClientTests.cs
+++ ConsoleClientUnitTests/ConnectionTests/SignalRClientTests.cs
@@ -1,23 +1,27 @@
-﻿using ConsoleClient.Connection;
+﻿using ConsoleClient;
+using ConsoleClient.Connection;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleClientUnitTests.ConnectionTests
 {
     [TestClass]
     public class SignalRClientTests
     {
-        //[TestMethod]
-        //public async Task Connect_ShouldStartHubConnections()
-        //{
-        //    // Arrange
-        //    var signalRClient = new SignalRClient();
+        [TestMethod]
+        public async Task Connect_ShouldStartHubConnections()
+        {
+            // Arrange
+            var configuration = new ConfigVars(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build());
 
-        //    // Act
-        //    await signalRClient.Connect();
+            var signalRClient = new SignalRClient(configuration);
 
-        //    //Assert
-        //    Assert.IsTrue(signalRClient._playerHubConnection.State == HubConnectionState.Connected);
-        //    Assert.IsTrue(signalRClient._caveHubConnection.State == HubConnectionState.Connected);
-        //}
+            // Act
+            await signalRClient.Connect();
+
+            //Assert
+            Assert.IsTrue(signalRClient._playerHubConnection.State == HubConnectionState.Connected);
+            Assert.IsTrue(signalRClient._caveHubConnection.State == HubConnectionState.Connected);
+        }
     }
 }
